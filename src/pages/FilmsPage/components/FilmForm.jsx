44439d9643d8a71f30a1066e285b81debbf1af99
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-10-04 22:18:19 +0300

    Start home work

---
+++ src/pages/FilmsPage/components/FilmForm.jsx
@@ -0,0 +1,179 @@
+import React, {Component} from "react";
+
+const initialData = {
+  title: "",
+  img: "",
+  description: "",
+  director: "",
+  price: "",
+  duration: "",
+  featured: false,
+};
+
+class FilmForm extends Component {
+  state = {
+    data: initialData,
+  };
+
+  handleStringChange = e =>
+    this.setState({
+      data: {...this.state.data, [e.target.name]: e.target.value},
+    });
+
+  handleNumberChange = e => {
+    let value = parseFloat(e.target.value);
+    value = isNaN(value) || value === 0 ? "" : Math.abs(value);
+    this.setState({
+      data: {...this.state.data, [e.target.name]: value},
+    });
+  };
+  handleCheckboxChange = e =>
+    this.setState({
+      data: {...this.state.data, [e.target.name]: e.target.checked},
+    });
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.data);
+  };
+
+  render() {
+    const {data} = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className="ui form">
+        <div className="ui grid mb-3">
+          {/* <!-- two column row START --> */}
+          <div className="two column row">
+            {/* <!-- left  column  START  --> */}
+            <div className="ten wide column">
+              {/* <!-- title START   --> */}
+              <div className="field error">
+                <label htmlFor="title">Film title</label>
+                <input
+                  value={data.title}
+                  onChange={this.handleStringChange}
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="film title"
+                />
+              </div>
+              {/* <!-- title END   --> */}
+
+              {/* <!-- image field START   --> */}
+              <div className="field img-grid">
+                <label htmlFor="img">Image</label>
+                <input
+                  value={data.img}
+                  onChange={this.handleStringChange}
+                  name="img"
+                />
+                <div className="inp-file">
+                  <label htmlFor="photo">Photo</label>
+                  <input type="file" id="photo" />
+                </div>
+              </div>
+              {/* <!-- image field END  --> */}
+            </div>
+            {/* <!-- left  column  END  --> */}
+
+            {/* <!-- right column START   --> */}
+            <div className="six wide column">
+              <img
+                src="http://via.placeholder.com/250x250"
+                className="ui image imgfit"
+                alt="uploaded"
+              />
+            </div>
+            {/* <!-- right column  END   --> */}
+          </div>
+          {/* <!--/ two column   --> */}
+
+          {/* <!-- description START   --> */}
+          <div className="column row field">
+            <label htmlFor="description">Film description</label>
+            <textarea
+              value={data.description}
+              onChange={this.handleStringChange}
+              name="description"
+              id="description"
+              placeholder="film description"
+            ></textarea>
+          </div>
+          {/* <!-- description END   -->  */}
+
+          {/* <!--  three column row START --> */}
+          <div className="three column row">
+            {/* <!-- director START --> */}
+            <div className="column field">
+              <label htmlFor="director">Director</label>
+              <input
+                value={data.director}
+                onChange={this.handleStringChange}
+                type="text"
+                name="director"
+                id="director"
+                placeholder="film director"
+              />
+            </div>
+            {/* <!--  director END  --> */}
+
+            {/* <!--   duration START --> */}
+            <div className="column field">
+              <label htmlFor="duration">Duration</label>
+              <input
+                value={data.duration}
+                onChange={this.handleNumberChange}
+                type="number"
+                name="duration"
+                id="duration"
+                placeholder="Duration"
+              />
+            </div>
+            {/* <!-- duration  END--> */}
+
+            {/* <!--  price START  --> */}
+            <div className="column field">
+              <label htmlFor="price">Price</label>
+              <input
+                value={data.price}
+                onChange={this.handleNumberChange}
+                type="number"
+                name="price"
+                id="price"
+                placeholder="price"
+              />
+            </div>
+            {/* <!--  price  END--> */}
+          </div>
+          {/* <!-- / three column row END  --> */}
+
+          {/* <!--  featured START --> */}
+          <div className="six wide column inline field">
+            <label htmlFor="featured">Featured</label>
+            <input
+              value={data.featured}
+              onChange={this.handleCheckboxChange}
+              type="checkbox"
+              name="featured"
+              id="featured"
+            />
+          </div>
+          {/* <!--  featured END --> */}
+
+          {/* <!-- Buttons START --> */}
+          <div className="ui fluid buttons">
+            <button className="ui button primary" type="submit">
+              Save
+            </button>
+            <div className="or"></div>
+            <span className="ui button">Hide form</span>
+          </div>
+          {/* <!-- Buttons  END --> */}
+        </div>
+      </form>
+    );
+  }
+}
+
+export default FilmForm;
---
+++ src/pages/FilmsPage/components/FilmList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FilmCard from "pages/FilmsPage/components/FilmCard";
+import Messages from "components/Messages";
+
+const FilmList = ({films}) => {
+  return (
+    <div className="ui four cards">
+      {films.length === 0 ? (
+        <Messages type="info" color="olive">
+          No fims yet in our database
+        </Messages>
+      ) : (
+        films.map(film => <FilmCard key={film._id} film={film} />)
+      )}
+    </div>
+  );
+};
+
+FilmList.defaultProps = {
+  films: [],
+};
+
+FilmList.propTypes = {
+  films: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default FilmList;
