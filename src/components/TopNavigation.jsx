9dc34b6ee0836809ed7ac8632d7abf18f8d8fbe5
KostiantynKushch <kostiantyn.kushch@gmail.com>
2020-10-20 20:53:11 +0300

    router

--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 import PropTypes from "prop-types";
 
-const TopNavigation = ({showForm}) => {
-  return (
-    <div className="ui secondary pointing menu">
-      <a href="/" className="item">
-        Home
-      </a>
-      <span onClick={showForm} className="item">
-        <i className="icon plus"></i>
+const TopNavigation = ({ showForm }) => {
+	return (
+		<div className="ui secondary pointing menu">
+			<NavLink exact to="/" className="item">
+				<i className="icon home" />Home
+      </NavLink>
+
+			<NavLink exact to="/films" className="item" >
+				<i className="icon films" /> Films
+		</NavLink>
+
+			<NavLink exact to="films/new" className="item">
+				<i className="icon plus" />
         Add new film
-      </span>
-    </div>
-  );
+      </NavLink>
+		</div>
+	);
 };
 
 TopNavigation.propTypes = {
-  showForm: PropTypes.func.isRequired,
+
 };
 
 export default TopNavigation;
